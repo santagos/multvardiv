07336cd3ebc76222f8b74a5011da10cddd44a402
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2025-12-11 11:49:35 +0100

    Graphics changed

--- _append_2_3.R
+++ _append_2_3.R
@@ -8,21 +8,16 @@ Sigma2 <- rbind(c(1, 0.3, 0.1), c(0.3, 1, 0.4), c(0.1, 0.4, 1))
 S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
 
 # Rényi divergence
-times <- iter <- numeric(length = length(S))
+renyitimes <- iter <- numeric(length = length(S))
 for (i in 1:length(S)) {
   s <- S[i]
-  times[i] <- system.time(
+  renyitimes[i] <- system.time(
     d <- diststudent(nu1 = nu, Sigma1 = s*Sigma, nu2 = nu, Sigma2 = Sigma,
                      dist = "renyi", bet = 0.25)
   )["elapsed"]
   iter[i] <- attr(d, "k")
 }
-
-# Plot of the execution times
-renyi_exetime <- data.frame(s = S, iter = iter, exetime = times)
-plot(exetime~s, data = renyi_exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Execution time")
-axis(side = 1, at = 1, labels = 1, cex.axis = 1.5)
+renyi <- data.frame(s = S, renyi_iter = iter, renyi_exetime = renyitimes)
 
 
 # Kullback-Leibler divergence
@@ -32,21 +27,30 @@ nu <- 2
 Sigma2 <- matrix(c(1, 0.3, 0.1, 0.3, 1, 0.4, 0.1, 0.4, 1), nrow = 3)
 
 # Values of the proportionality coefficient
-S <- c(seq(0.1, 1, by = 0.1), seq(2, 50, by = 2))
+S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
 
 # Kullback-Leibler divergence
-times <- iter <- numeric(length = length(S))
+kltimes <- iter <- numeric(length = length(S))
 for (i in 1:length(S)) {
   s <- S[i]
-  times[i] <- system.time(
+  kltimes[i] <- system.time(
     d <- kld(nu1 = nu, Sigma1 = s*Sigma, nu2 = nu, Sigma2 = Sigma,
              distribution = "mtd")
   )["elapsed"]
   iter[i] <- attr(d, "k")
 }
+kl <- data.frame(s = S, kl_iter = iter, kl_exetime = kltimes)
+
 
 # Plot of the execution times
-kl_exetime <- data.frame(s = S, iter = iter, exetime = times)
-plot(exetime~s, data = kl_exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Execution time")
-axis(side = 1, at = 1, labels = 1, cex.axis = 1.5)
+
+# Plot of the execution times
+exetime <- merge(renyi, kl)
+matplot(x = exetime$s,
+        y = cbind(exetime$renyi_iter, exetime$kl_iter),
+        type = "bb", pch = 16, lty = 1, xlab = "s", ylab = "Execution time")
+legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
+matplot(x = exetime$s,
+        y = cbind(exetime$renyi_exetime, exetime$kl_exetime),
+        type = "bb", pch = 16, lty = 1, xlab = "s", ylab = "Execution time")
+legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
