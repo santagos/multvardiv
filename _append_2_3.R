5f4944330fd57fa0f7ec06b004264d74b10f62be
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2025-12-11 12:45:36 +0100

    Graphs: now among the computed divergence (and not among s)

--- _append_2_3.R
+++ _append_2_3.R
@@ -8,16 +8,18 @@ Sigma2 <- rbind(c(1, 0.3, 0.1), c(0.3, 1, 0.4), c(0.1, 0.4, 1))
 S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
 
 # Rényi divergence
-renyitimes <- iter <- numeric(length = length(S))
+renyitimes <- iter <- renyidiv <- numeric(length = length(S))
 for (i in 1:length(S)) {
   s <- S[i]
   renyitimes[i] <- system.time(
     d <- diststudent(nu1 = nu, Sigma1 = s*Sigma, nu2 = nu, Sigma2 = Sigma,
                      dist = "renyi", bet = 0.25)
   )["elapsed"]
+  renyidiv[i] <- d
   iter[i] <- attr(d, "k")
 }
-renyi <- data.frame(s = S, renyi_iter = iter, renyi_exetime = renyitimes)
+renyi <- data.frame(s = S, renyi_iter = iter, renyi_exetime = renyitimes,
+                    renyi_div = renyidiv)
 
 
 # Kullback-Leibler divergence
@@ -30,27 +32,33 @@ Sigma2 <- matrix(c(1, 0.3, 0.1, 0.3, 1, 0.4, 0.1, 0.4, 1), nrow = 3)
 S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
 
 # Kullback-Leibler divergence
-kltimes <- iter <- numeric(length = length(S))
+kltimes <- iter <- kldiv <- numeric(length = length(S))
 for (i in 1:length(S)) {
   s <- S[i]
   kltimes[i] <- system.time(
     d <- kld(nu1 = nu, Sigma1 = s*Sigma, nu2 = nu, Sigma2 = Sigma,
              distribution = "mtd")
   )["elapsed"]
+  kldiv[i] <- d
   iter[i] <- attr(d, "k")
 }
-kl <- data.frame(s = S, kl_iter = iter, kl_exetime = kltimes)
+kl <- data.frame(s = S, kl_iter = iter, kl_exetime = kltimes,
+                 kl_div = kldiv)
 
-
-# Plot of the execution times
 
 # Plot of the execution times
 exetime <- merge(renyi, kl)
-matplot(x = exetime$s,
-        y = cbind(exetime$renyi_iter, exetime$kl_iter),
-        type = "bb", pch = 16, lty = 1, xlab = "s", ylab = "Execution time")
-legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
-matplot(x = exetime$s,
-        y = cbind(exetime$renyi_exetime, exetime$kl_exetime),
-        type = "bb", pch = 16, lty = 1, xlab = "s", ylab = "Execution time")
-legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
+plot(renyi_iter~renyi_div, data = exetime, type = "b", pch = 16,
+     main = "Rényi", xlab = "Divergence", ylab = "Number of iterations")
+plot(kl_iter~kl_div, data = exetime, type = "b", pch = 16,
+     main = "Kullback-Leibler", xlab = "Divergence", ylab = "Number of iterations")
+plot(renyi_exetime~renyi_div, data = exetime, type = "b", pch = 16,
+     main = "Rényi", xlab = "Divergence", ylab = "Execution time")
+plot(kl_exetime~kl_div, data = exetime, type = "b", pch = 16,
+     main = "Kullback-Leibler", xlab = "Divergence", ylab = "Execution time")
+
+# legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
+# matplot(x = exetime$kl_div,
+#         y = cbind(exetime$renyi_exetime, exetime$kl_exetime), type = "bb",
+#         pch = 16, lty = 1, xlab = "Divergence", ylab = "Execution time")
+# legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
