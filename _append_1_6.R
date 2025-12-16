6e445f30a9e21246d12987a13617e839e2a9fd16
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2025-12-16 18:14:55 +0100

    MGGD, adding the study of the divergence

---
+++ _append_1_6.R
@@ -0,0 +1,27 @@
+s <- 3
+
+# Values of Sigma1[1, 1]
+s1 <- c(seq(0.1, s, by = 0.05), seq(s+1, 1000, by = 1))
+
+# The other parameters
+Sigma1 <- Sigma2 <- matrix(c(s, 0.3, 0.3, 1), nrow = 2)
+Sigma1[1, 1] <- NA
+
+bet <- 0.74
+
+# Kullback-Leibler divergence
+kullback_leibler <- sapply(s1, function(s1) {
+  Sigma1[1, 1] <- s1
+  kld(Sigma1 = Sigma1, Sigma2 = Sigma2, beta1 = bet, beta2 = bet,
+      distribution = "mggd")
+})
+
+# Table of the results
+divS1 <- data.frame(
+  s1 = s1,
+  kullback_leibler = kullback_leibler
+)
+
+plot(kullback_leibler~s1, data = divS1, type = "b", pch = 16,
+     xlab = substitute(Sigma[1][1]), ylab = "Divergence")
+axis(side = 1, at = Sigma2[1, 1], labels = substitute(Sigma[1][1]), cex.axis = 1.5)
---
+++ _append_1_7.R
@@ -0,0 +1,22 @@
+# The parameters
+Sigma <- matrix(c(1, 0.3, 0.3, 1), nrow = 2)
+bet <- 0.74
+
+# Values of the proportionality coefficient
+s <- c(seq(0.05, 1, by = 0.05), seq(2, 600, by = 2))
+
+# Kullback-Leibler divergence
+kullback_leibler <- sapply(s, function(s) {
+  kld(Sigma1 = s*Sigma, Sigma2 = Sigma, beta1 = bet, beta2 = bet,
+      distribution = "mggd")
+})
+
+# Table of the results
+divSigma <- data.frame(
+  facteur = s,
+  kullback_leibler = kullback_leibler
+)
+
+plot(kullback_leibler~facteur, data = divSigma, type = "b", pch = 16,
+     xlab = "s", ylab = "Divergence")
+axis(side = 1, at = 1, labels = 1, cex.axis = 1.5)
