98a5cc15638ad35f0a6472eafbbf89e70da214ad
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2025-12-17 11:43:50 +0100

    MGGD: variation of the KLD according to beta1

---
+++ _append_1_8.R
@@ -0,0 +1,25 @@
+bet <- 0.74
+
+# Values of beta1
+beta1 <- c(seq(0.05, nu, by = 0.01), seq(nu+1, 22, by = 2))
+
+# Fixed values of the other parameters
+beta2 <- bet
+Sigma <- matrix(c(1, 0.3, 0.3, 1), nrow = 2)
+
+# Kullback-Leibler divergence
+kullback_leibler <- sapply(beta1, function(b) {
+  kld(Sigma1 = Sigma, Sigma2 = Sigma,
+      beta1 = b, beta2 = beta2,
+      distribution = "mggd")
+})
+
+# Table of the results
+divbeta <- data.frame(
+  beta1 = beta1,
+  kullback_leibler = kullback_leibler
+)
+
+plot(kullback_leibler~beta1, data = divbeta, type = "b", pch = 16,
+     xlab = substitute(beta[1]), ylab = "Divergence", ylim = c(-0.1, 4))
+axis(side = 1, at = beta2, labels = substitute(beta[2]), cex.axis = 1.5)
