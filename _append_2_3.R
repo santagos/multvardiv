5b2929392148204bd269d7aca7c4f77767b2563d
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2026-09-03 20:24:11 +0200

    Avoid repeating some lines. Remove some commentaries.

--- _append_2_3.R
+++ _append_2_3.R
@@ -1,11 +1,10 @@
-# Rényi divergence
-
-# Degrees of freedom
+# Parameters
 nu <- 2
 Sigma2 <- rbind(c(1, 0.3, 0.1), c(0.3, 1, 0.4), c(0.1, 0.4, 1))
 
 # Values of the proportionality coefficient
 S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
+
 
 # Rényi divergence
 renyitimes <- iter <- renyidiv <- numeric(length = length(S))
@@ -21,15 +20,6 @@ for (i in 1:length(S)) {
 renyi <- data.frame(s = S, renyi_iter = iter, renyi_exetime = renyitimes,
                     renyi_div = renyidiv)
 
-
-# Kullback-Leibler divergence
-
-# Degrees of freedom
-nu <- 2
-Sigma2 <- matrix(c(1, 0.3, 0.1, 0.3, 1, 0.4, 0.1, 0.4, 1), nrow = 3)
-
-# Values of the proportionality coefficient
-S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
 
 # Kullback-Leibler divergence
 kltimes <- iter <- kldiv <- numeric(length = length(S))
@@ -64,9 +54,3 @@ plot(kl_exetime~s, data = exetime, type = "b", pch = 16,
      xlab = "s", ylab = "Execution time (s)")
 plot(kl_div~s, data = exetime, type = "b", pch = 16,
      xlab = "s", ylab = "Computed divergence")
-
-# legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
-# matplot(x = exetime$kl_div,
-#         y = cbind(exetime$renyi_exetime, exetime$kl_exetime), type = "bb",
-#         pch = 16, lty = 1, xlab = "Divergence", ylab = "Execution time")
-# legend("topleft", legend = c("Rényi", "Kullback-Leibler"), col = 1:2, pch = 16)
