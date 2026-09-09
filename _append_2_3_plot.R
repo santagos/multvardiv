2ea0dc39c7bee5114d52692805a31080570fbdd8
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2026-09-09 19:42:07 +0200

    Reads the values in a RDS file, then plots the graph

---
+++ _append_2_3_plot.R
@@ -0,0 +1,19 @@
+exetime <- readRDS("renyikl_2_3.rds")
+
+# Plot of the execution times
+
+par(mfrow = c(2, 2))
+plot(renyi_iter~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Number of iterations")
+plot(renyi_exetime~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Execution time (s)")
+plot(renyi_div~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Computed divergence")
+
+par(mfrow = c(2, 2))
+plot(kl_iter~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Number of iterations")
+plot(kl_exetime~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Execution time (s)")
+plot(kl_div~s, data = exetime, type = "b", pch = 16,
+     xlab = "s", ylab = "Computed divergence")
