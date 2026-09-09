61f9f9d3a21bc5220db9e2ad1d93fc71df7067ce
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2026-09-09 19:41:37 +0200

    Plot removed, results saved into a RDS file

--- _append_2_3.R
+++ _append_2_3.R
@@ -36,21 +36,7 @@ kl <- data.frame(s = S, kl_iter = iter, kl_exetime = kltimes,
                  kl_div = kldiv)
 
 
-# Plot of the execution times
 exetime <- merge(renyi, kl)
 
-par(mfrow = c(2, 2))
-plot(renyi_iter~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Number of iterations")
-plot(renyi_exetime~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Execution time (s)")
-plot(renyi_div~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Computed divergence")
 
-par(mfrow = c(2, 2))
-plot(kl_iter~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Number of iterations")
-plot(kl_exetime~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Execution time (s)")
-plot(kl_div~s, data = exetime, type = "b", pch = 16,
-     xlab = "s", ylab = "Computed divergence")
+saveRDS(exetime, file = "renyikl_2_3.rds")
