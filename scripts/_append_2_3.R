9aadcab3831c6304f9c1b5788c5a3782440171e6
Pierre Santagostini <pierre.santagostini@agrocampus-ouest.fr>
2026-09-10 14:35:56 +0200

    Sigma (not Sigma2)

--- scripts/_append_2_3.R
+++ scripts/_append_2_3.R
@@ -1,6 +1,6 @@
 # Parameters
 nu <- 2
-Sigma2 <- rbind(c(1, 0.3, 0.1), c(0.3, 1, 0.4), c(0.1, 0.4, 1))
+Sigma <- rbind(c(1, 0.3, 0.1), c(0.3, 1, 0.4), c(0.1, 0.4, 1))
 
 # Values of the proportionality coefficient
 S <- c(seq(0.1, 1, by = 0.1), seq(2, 70, by = 2))
